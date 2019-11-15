35311fb5f96425b9d66e51e5d8272b988a24466e
logan <Logan.Fockler@floodcitygames.com>
2019-11-15 14:31:03 -0500

    Logans Second Commit

--- PizzaOrder/PizzaOrder/Controllers/HomeController.cs
+++ PizzaOrder/PizzaOrder/Controllers/HomeController.cs
@@ -33,6 +33,7 @@ namespace PizzaOrder.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            //logans second comment
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
