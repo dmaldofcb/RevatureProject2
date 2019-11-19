c212839375267ed0740920b6adfaeee635f82520
dmaldofcb <damald1@umbc.edu>
2019-11-19 01:25:44 -0500

    moved everything into pizza order

---
+++ PizzaOrder/PizzaOrderAPI/Controllers/OrdersAPIController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Layers.Models.Models;
+using Layers.Models.Repository;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PizzaOrder.Data;
+
+namespace PizzaOrderAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrdersAPIController : ControllerBase
+    {
+        private readonly IOrder _repo;
+        // GET: api/Orders
+
+        public OrdersAPIController(IOrder repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _repo.Get();
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await _repo.Get(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+
+        // PUT: api/Orders/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutOrder(int id, Order order)
+        //{
+        //    if (id != order.Id)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(order).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!OrderExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        // POST: api/Orders
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            await _repo.Create(order);
+
+            return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+        }
+
+        // DELETE: api/Orders/5
+        //[HttpDelete("{id}")]
+        //public async Task<ActionResult<Order>> DeleteOrder(int id)
+        //{
+        //    var order = await _context.Orders.FindAsync(id);
+        //    if (order == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _context.Orders.Remove(order);
+        //    await _context.SaveChangesAsync();
+
+        //    return order;
+        //}
+
+    
+    }
+}
