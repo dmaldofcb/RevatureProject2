7f3b0bf1e653f6b344e4eee842a6fbecc974a21e
Raymond <pickettr5@students.rowan.edu>
2019-11-22 15:19:20 -0500

    Added APIs for crust, toppings, and size.

---
+++ PizzaOrder/PizzaOrder/Controllers/CrustAPIController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Layers.Models.Models;
+using Layers.Models.Repository;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using PizzaOrder.Data;
+
+namespace PizzaOrder.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CrustAPIController : ControllerBase
+    {
+        private readonly ICrust _repo;
+
+        public CrustAPIController(ICrust repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Crust>> GetCrust(int id)
+        {
+
+            var crust = await _repo.Get(id);
+
+            if (crust == null)
+            {
+                return NotFound();
+            }
+
+            return crust;
+        }
+    }
+}
---
+++ PizzaOrder/PizzaOrder/Controllers/SizeAPIController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Layers.Models.Models;
+using Layers.Models.Repository;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using PizzaOrder.Data;
+
+namespace PizzaOrder.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SizeAPIController : ControllerBase
+    {
+        private readonly ISize _repo;
+
+        public SizeAPIController(ISize repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Size>> GetSize(int id)
+        {
+
+            var size = await _repo.Get(id);
+
+            if (size == null)
+            {
+                return NotFound();
+            }
+
+            return size;
+        }
+    }
+}
