e136c9918768b5b51151c43d3f77e32f49d76926
Raymond <pickettr5@students.rowan.edu>
2019-11-22 13:44:58 -0500

    Added stuff to the Repository folder.

---
+++ PizzaOrder/Pizza.Models/Repository/ISize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Layers.Models.Models;
+
+namespace Layers.Models.Repository
+{
+    public interface ISize
+    {
+        Task<Size> Get(int? id);
+        Task<List<Size>> Get(string user);
+    }
+}
---
+++ PizzaOrder/Pizza.Models/Repository/Toppings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Layers.Models.Models;
+
+namespace Layers.Models.Repository
+{
+    public interface IToppings
+    {
+        Task<Toppings> Get(int? id);
+        Task<List<Toppings>> Get(string user);
+    }
+}
